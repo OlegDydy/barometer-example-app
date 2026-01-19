9f52daed1511d87a360c0380c7b934eeb81f9585
Oleg Dydykin <dydykin.oleg2013@gmail.com>
2026-01-19 18:09:28 +0300

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { computed, reactive, useTemplateRef, watchEffect } from 'vue';
+import { computed, reactive, ref, useTemplateRef, watchEffect } from 'vue';
 
 const canvas = useTemplateRef('canvas');
 const ctx = computed(() => canvas.value?.getContext('2d') || null);
 
-const controlPoints = reactive(<number>[0, 1, 2, 3]);
+const controlPoints = reactive([0, 1, 2, 3]);
 const size = 4;
+
+type Vector2Like = [x: number, y: number];
+
+function lagrange() {
+  
+}
 
 const SEGMENTS = 128;
 function ellipse(rx: number, ry = 1): number {
@@ -17,31 +23,35 @@ function ellipse(rx: number, ry = 1): number {
     const nx = rx * Math.cos(a);
     const ny = ry * Math.sin(a);
     sum += Math.hypot(nx - x, ny - y);
+    x = nx;
+    y = ny;
   }
   return sum * 4;
 }
 
 function approx(a: number, b = 1, chain = [1, 1]): number {
   const ratio = a / b;
-  // return b * 4 * (1 / (ratio + 1) + ratio);
+  // return b * 4 * (ratio + 1 / (ratio + 1));
+  // return b * 4 * (ratio + 1 / (ratio + (1 / (ratio + (1 / (ratio + 1))))));
+  // b * 4 * (ratio + 1 / (ratio + 1))
   const last = chain.length - 1;
   let sum = chain[last];
   for (let i = last; i--; ) {
     sum = chain[i] / (ratio + sum);
   }
-  return 4 * b * sum;
+  return 4 * b * (ratio + sum);
 }
 
 function fix(x: number) {
   if (x > 2) return 0;
 
-  return ((0.006600646426239134 * x - 0.04658177262052619) * x + 0.1111427211658603) * x;
+  return 4*((0.006600646426239134 * x - 0.04658177262052619) * x + 0.1111427211658603) * x;
 }
 
-type Vector2Like = [x: number, y: number];
 const ticks = 128;
 const xRange: Vector2Like = [0, 10];
 const colors = ['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f'];
+const refresh = ref(0);
 
 function updateRange(range: Vector2Like, value): Vector2Like {
   if (value < range[0]) range[0] = value;
@@ -50,7 +60,9 @@ function updateRange(range: Vector2Like, value): Vector2Like {
 }
 
 function plot(ctx: CanvasRenderingContext2D) {
-  const values: number[][] = [[], []];
+  refresh.value;
+
+  const values: [number[], number[]] = [[], []];
   const yRange: Vector2Like = [+Infinity, -Infinity];
   for (let i = 0; i <= ticks; ++i) {
     const x = xRange[0] + (xRange[1] - xRange[0]) * (i / ticks);
@@ -58,19 +70,66 @@ function plot(ctx: CanvasRenderingContext2D) {
     let y = ellipse(x) - approx(x);
     values[0][i] = y;
     updateRange(yRange, y);
-    
+
     y = fix(x);
-    values[0][i] = y;
+    values[1][i] = y;
     updateRange(yRange, y);
   }
-  
-  let step = Math.log10();
+
+  let step = Math.log10(yRange[1] - yRange[0]);
+  if (step < 0) {
+    step = Math.ceil(-step);
+    step = 1 / 10 ** step / 2;
+  } else {
+    step = Math.ceil(step);
+    step = 10 ** step / 2;
+  }
+
+  // render
+  // round
+  yRange[0] = Math.floor(yRange[0] / step) * step;
+  yRange[1] = Math.ceil(yRange[1] / step) * step;
+
+  // step = 0.05;
+  // yRange[0] = -0.10;
+  // yRange[1] = 0.35;
+
+  const { width, height } = ctx.canvas;
+  const scaleY = height / (yRange[1] - yRange[0]);
+  const scaleX = width / (xRange[1] - xRange[0]);
+  ctx.clearRect(0, 0, width, height);
+
+  for (let i = 0; i < values.length; ++i) {
+    const row = values[i] as number[];
+
+    let x = xRange[0];
+    let y = (row[0] as number) - yRange[0];
+    ctx.beginPath();
+    ctx.moveTo(x * scaleX, height - y * scaleY);
+    for (let j = 1; j <= ticks; ++j) {
+      x = xRange[0] + (xRange[1] - xRange[0]) * (j / ticks);
+      y = (row[j] as number) - yRange[0];
+
+      ctx.lineTo(x * scaleX, height - y * scaleY);
+    }
+    ctx.strokeStyle = colors[i % colors.length] as string;
+    ctx.stroke();
+  }
 }
 
-watchEffect(() => {});
+watchEffect(() => {
+  if (!ctx.value) return;
+
+  plot(ctx.value);
+});
+
+function invalidate() {
+  refresh.value++;
+}
 </script>
 
 <template>
+  <button @click="invalidate">render</button>
   <canvas ref="canvas" width="1280" height="720"></canvas>
 </template>
 
